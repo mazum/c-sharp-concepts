40d936ae7f6c84215483a8cd56f0a1a8dd143d63
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-11-03 01:05:39 +1100

    Soap Serialization of Property

---
+++ SoapSerializationOfProperty/SoapSerializationOfProperty/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SoapSerializationOfProperty
+{
+    [Serializable]
+    public class Car
+    {
+        public bool IsHatchBack;
+
+        //To prove Property are not serialized. Only the underlying private variable is serialized. This is fine.
+        private decimal _price;
+        public decimal Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+    }
+}
