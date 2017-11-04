8c732819d119f7a01a8518a8b3560e515d0157dd
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-11-04 23:36:08 +1100

    Soap Serialization of Automatic Property.

---
+++ SoapSerializationOfAutomaticProperty/SoapSerializationOfAutomaticProperty/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SoapSerializationOfAutomaticProperty
+{
+    [Serializable]
+    public class Car
+    {
+        public bool IsHatchBack;
+
+        //Automatic Properties should never be deserialized. As we can't guarantee the deserialized property name. Random name is generated.
+        public decimal Price { get; set; }
+    }
+}
---
+++ SoapSerializationOfAutomaticProperty/SoapSerializationOfAutomaticProperty/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Soap;
+
+namespace SoapSerializationOfAutomaticProperty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stream stream = Serialize();
+
+            Console.WriteLine(new StreamReader(stream).ReadToEnd());
+
+            stream.Position = 0;
+
+            Deserialize(stream);
+
+            Console.ReadLine();
+        }
+
+        private static Stream Serialize()
+        {
+            Car car = new Car { IsHatchBack = false, Price = 20000 };
+            
+            MemoryStream stream = new MemoryStream();
+
+            SoapFormatter formatter = new SoapFormatter();
+            formatter.Serialize(stream, car);
+            
+            stream.Position = 0;
+
+            return stream;
+        }
+
+        private static void Deserialize(Stream stream)
+        {
+            SoapFormatter formatter = new SoapFormatter();
+            Car car = (Car)formatter.Deserialize(stream);
+        }
+    }
+}
