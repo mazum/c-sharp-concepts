f372482aa1eb63f0d4587cf5ce9da1f7bee0bfd7
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-11-24 23:14:37 +1100

    Serialization Hooks.

---
+++ SerializationHooks/App/Data/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    [Serializable]
+    class Car
+    {
+        public string RentedBy;
+        public int RentDays;
+
+        [NonSerialized] private int RentalRate;
+
+        [NonSerialized] private int TotalRent;
+
+        public Car()
+        {
+            //Read rate from a web service
+            RentalRate = 50;
+        }
+
+        [OnSerializing]
+        private void OnSerializing(StreamingContext context)
+        {
+            //For some reason, this is secret stuff!
+            RentedBy = "encrypted";
+        }
+
+        [OnSerialized]
+        private void OnSerialized(StreamingContext context)
+        {
+            RentedBy = "decrypted";
+        }
+
+        [OnDeserializing]
+        private void OnDeserializing(StreamingContext context)
+        {
+            //Read rate from a web service
+            RentalRate = 50;
+        }
+
+        [OnDeserialized]
+        private void OnDeserialized(StreamingContext context)
+        {
+            TotalRent = RentalRate * RentDays;
+        }
+    }
+}
---
+++ SerializationHooks/App/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car=new Car();
+
+            car.RentDays = 5;
+            car.RentedBy = "Aby";
+
+            MemoryStream ms = new MemoryStream();
+
+            BinaryFormatter formatter=new BinaryFormatter();
+            formatter.Serialize(ms, car);
+
+            ms.Position = 0;
+
+            Car car1 = (Car) formatter.Deserialize(ms);
+
+            Console.ReadLine();
+        }
+    }
+}
