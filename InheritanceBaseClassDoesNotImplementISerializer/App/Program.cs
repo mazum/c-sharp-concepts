a59da8c3e5f37c196419f56d1551cd15a3cd3444
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-11-30 21:15:58 +1100

    Program.cs is updated.

--- InheritanceBaseClassDoesNotImplementISerializer/App/Program.cs
+++ InheritanceBaseClassDoesNotImplementISerializer/App/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,20 @@ namespace App
     {
         static void Main(string[] args)
         {
+            Car car = new Car();
+            car.IsHatchback = false;
+            car.Model = 2014;
+            car.Type = "Chev";
+
+            MemoryStream ms = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(ms, car);
+
+            ms.Position = 0;
+
+            Car car1 = (Car) formatter.Deserialize(ms);
+
+            Console.ReadLine();
         }
     }
 }
