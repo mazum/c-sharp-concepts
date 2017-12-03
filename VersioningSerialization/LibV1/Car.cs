efd9bf74b68e6f3701aca67a7c479ead5900ccfb
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-12-03 19:30:41 +1100

    Versioning Serialization- LibV1

---
+++ VersioningSerialization/LibV1/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibV1
+{
+    [Serializable]
+    public class Car_V1
+    {
+        public string Type;
+        public int Model;
+        public bool IsHatchback;
+    }
+}
