2962f2ee4eb98b12b778c4cc331808606084ffec
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-12-04 23:08:55 +1100

    Versioning Serialization- LibV2

---
+++ VersioningSerialization/LibV2/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibV2
+{
+    [Serializable]
+    public class Car_V2: ISerializable
+    {
+        public string Type;
+        public int Model;
+        public int IsHatchback;
+
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Type", Type);
+            info.AddValue("Model", Model);
+        }
+
+        private Car_V2(SerializationInfo info, StreamingContext context)
+        {
+            Model = info.GetInt32("Model");
+
+            if (info.GetString("Type") == "Chev")
+                Type = "CH";
+            else Type = info.GetString("Type");
+
+            IsHatchback = info.GetInt32("IsHatchback");
+        }
+    }
+}
