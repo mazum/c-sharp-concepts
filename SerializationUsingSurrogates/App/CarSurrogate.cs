a3bc4e17148eb7937654827c7e82ce7b1439830b
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-12-06 21:30:07 +1100

    Serialization Using Surrogates- Initial check in.

---
+++ SerializationUsingSurrogates/App/CarSurrogate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Lib;
+
+namespace App
+{
+    class CarSurrogate : ISerializationSurrogate
+    {
+        public void GetObjectData(object obj, SerializationInfo info, StreamingContext context)
+        {
+            Car car = (Car) obj;
+
+            info.AddValue("Model", car.Model);
+            info.AddValue("Type", car.Type);
+            info.AddValue("IsHatchback", car.IsHatchback);
+        }
+
+        public object SetObjectData(object obj, SerializationInfo info, StreamingContext context,
+            ISurrogateSelector selector)
+        {
+            Car car = (Car)obj;
+
+            car.Model = info.GetInt32("Model");
+            car.Type = info.GetString("Type");
+            car.IsHatchback = info.GetInt32("IsHatchback");
+
+            return car;
+
+            //or we could do the following to save "obj" and return null
+            //((SurrogateLib.Car)obj).Model = info.GetInt32("Model");
+            //((SurrogateLib.Car)obj).Type = info.GetString("Type");
+            //((SurrogateLib.Car)obj).IsHatchback = info.GetInt32("IsHatchback");
+            //return null;
+        }
+    }
+}
---
+++ SerializationUsingSurrogates/App/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using Lib;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Type = "Chev";
+            car.Model = 2015;
+            car.IsHatchback = 0;
+
+            MemoryStream stream = new MemoryStream();
+            BinaryFormatter formatter=new BinaryFormatter();
+
+            CarSurrogate carSurrogate = new CarSurrogate();
+
+            SurrogateSelector surrogateSelector = new SurrogateSelector();
+            surrogateSelector.AddSurrogate(typeof(Car), new StreamingContext(StreamingContextStates.All), carSurrogate);
+
+            formatter.SurrogateSelector = surrogateSelector;
+
+            formatter.Serialize(stream, car);
+
+            stream.Position = 0;
+
+            Car deserializedCar = (Car) formatter.Deserialize(stream);
+
+
+        }
+    }
+}
