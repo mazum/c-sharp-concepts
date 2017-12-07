671af1357a0fa6abb137ed77c3a748369ec3e5ec
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-12-07 20:45:35 +1100

    Serialization Using Surrogates- Lib project.

---
+++ SerializationUsingSurrogates/Lib/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    public class Car
+    {
+        public string Type;
+        public int Model;
+        public int IsHatchback;
+
+        public Car()
+        {
+        }
+    }
+}
